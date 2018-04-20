98c53746efed9a0082ecb3d8f4ac8abd0f4cf6a4
samrosenfield22 <samrosenfield22@gmail.com>
2018-04-20 03:50:32 -0400

    Fixed comment in softUart

--- include/softUart.h
+++ include/softUart.h
@@ -5,8 +5,8 @@
  pin being used (defined in debugSettings.h)
  * DELAYVAL must be defined -- it should be set to FOSC/baud (also defined in debugSettings.h)
 	* the user is required to calculate this value in order to reduce the softUart's memory overhead
- * any program wishing to use dputchar()/dprintf()/dprintnum() to send data over the TX line must
- first call softUartSetup()
+ * any program wishing to use dputchar()/dprintf()/other dlibc functions to send data over
+ the TX line must first call softUartSetup()
  * in order for assert() to automatically print debugging info, uncomment the #definition of
  USING_SOFT_UART (in debugSettings.h)
  
